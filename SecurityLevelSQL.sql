cda78cd8420d9ada4bfc55670c38812253796d24
Amani Khalid <amanikhalidd@gmail.com>
2025-06-01 11:42:30 +0400

    Created login and user for HR department

--- SecurityLevelSQL.sql
+++ SecurityLevelSQL.sql
@@ -4,3 +4,7 @@ CREATE DATABASE CompanyDB;
 -- Use the new database
 USE CompanyDB
 
+-- Create login and user for HR department
+CREATE LOGIN hr_login WITH PASSWORD = 'Hr@2299';
+CREATE USER hr_user FOR LOGIN hr_login;
+
